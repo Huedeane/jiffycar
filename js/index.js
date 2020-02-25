c44f31d77489dace5f6e16500dc2e48ac9d03fff
Hieu Dinh <hiiamaalien@gmail.com>
2020-02-25 13:48:45 -0500

    Save point

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var budgetController = (function(){
+
+
+})();
+
+var UIController = (function {
+
+
+
+})();
+
+
+var rental = function(id, year, make, model, seat, price, ){
+    this.id = id;
+    this.year = year;
+    this.make = make;
+    this.model = model;
+    this.seat = seat;
+    this.price = price;
+};
+
+
+
+
+
+
+
+var rentalData = [
+    {
+        'id':'124578', 
+        'year': '2011', 
+        'make':'lotus', 
+        'model':'evora', 
+        'seat':'5',
+        'price':'10', 
+        'history':[
+            {'dateFrom': '9/27', 'dateTo': '9/30', 'daysRented': '3', 'milesDriven': '47815',  }
+        ]
+    },
+
+
+
+
+]
